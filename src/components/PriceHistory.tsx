92be4a9cf631a3a2abf27949a0e41ab68c61feb4
aanmeba <aanmeba@gmail.com>
2023-09-12 16:58:34 +1000

    working on multi-line graph

--- src/components/PriceHistory.tsx
+++ src/components/PriceHistory.tsx
@@ -1,35 +1,102 @@
 import { Line } from "react-chartjs-2";
-import { GeneralisedHistoryTypeList } from "../common/types_interfaces";
+import {
+  // GeneralisedHistoryTypeList,
+  GeneralisedHistoryTypeObject,
+  HistoryCryptoKeys,
+  cryptos,
+} from "../common/types_interfaces";
 import { useEffect, useState } from "react";
 import { Chart, ChartData, registerables } from "chart.js";
+import ChartWrapper from "./ChartWrapper";
 
 Chart.register(...registerables);
 
-const PriceHistory = ({ cryptoData }: GeneralisedHistoryTypeList) => {
+const PriceHistory = ({ cryptoData }: GeneralisedHistoryTypeObject) => {
   const [result, setResult] = useState<ChartData<"line">>({ datasets: [] });
-  const configDataSet = {
-    label: "Price Tracker",
-    fill: false,
-  };
+  // const configDataSet = {
+  //   label: "Price Tracker",
+  //   fill: false,
+  // };
 
-  const labels = cryptoData.map((e) => e.date);
-  const dataInDatasets = cryptoData.map((c) => c.price);
+  console.log("cryptoData ---- ", cryptoData);
+
+  const labels = (key: HistoryCryptoKeys) => cryptoData[key].map((e) => e.date);
+  const dataInDatasets = (key: HistoryCryptoKeys) =>
+    cryptoData[key].map((c) => c.price);
 
   useEffect(() => {
     try {
-      setResult({
-        labels,
-        datasets: [{ data: dataInDatasets, ...configDataSet }],
+      cryptos.forEach((crypto, i) => {
+        console.log(crypto, " ** crypto");
+        const newDataset = {
+          label: crypto,
+          data: dataInDatasets(crypto as HistoryCryptoKeys),
+          yAxisID: `y${i}`,
+        };
+
+        const newDatasets = [...result.datasets, newDataset];
+        setResult({
+          labels: labels(crypto as HistoryCryptoKeys),
+          datasets: newDatasets,
+        });
+        // setResult({
+        //   labels: labels(crypto as HistoryCryptoKeys),
+        //   datasets: [
+        //     ...result.datasets,
+        //     {
+        //       label: crypto,
+        //       data: dataInDatasets(crypto as HistoryCryptoKeys),
+        //       yAxisID: `y${i}`,
+        //     },
+        //   ],
+        // });
       });
     } catch (err) {
       console.log("Caught error", err);
     }
   }, []);
 
+  console.log(result, " ---- result ");
+  // const config = {
+  //   type: "line",
+  //   data: data,
+  //   options: {
+  //     responsive: true,
+  //     interaction: {
+  //       mode: "index",
+  //       intersect: false,
+  //     },
+  //     stacked: false,
+  //     plugins: {
+  //       title: {
+  //         display: true,
+  //         text: "Chart.js Line Chart - Multi Axis",
+  //       },
+  //     },
+  //     scales: {
+  //       y0: {
+  //         type: "linear",
+  //         display: true,
+  //         position: "left",
+  //       },
+  //       y1: {
+  //         type: "linear",
+  //         display: true,
+  //         position: "right",
+
+  //         // grid line settings
+  //         grid: {
+  //           drawOnChartArea: false, // only want the grid lines for one axis to show up
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
+
   return (
-    <>
+    <ChartWrapper title="Price Tracker" variant="h5">
       <Line data={result} />
-    </>
+    </ChartWrapper>
   );
 };
 
--- src/containers/HistoryContainer.tsx
+++ src/containers/HistoryContainer.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import PriceHistory from "../components/PriceHistory";
 import { getHistoryById } from "../services/api-services";
 import { filterHistory, getLast90Days } from "../helpers/helpers";
-import { GeneralisedHistoryType } from "../common/types_interfaces";
+import {
+  GeneralisedHistoryTypeData,
+  cryptos,
+} from "../common/types_interfaces";
+import { Grid } from "@mui/material";
 
 const HistoryContainer = () => {
   const initialValue = [
@@ -11,27 +15,41 @@ const HistoryContainer = () => {
       date: "",
     },
   ];
+
+  const initialObjectValue = cryptos.reduce((prev, curr) => {
+    return { ...prev, [curr]: initialValue };
+  }, {} as GeneralisedHistoryTypeData);
+
   const [fetchStatus, setFetchStatus] = useState<boolean>(false);
   const [fetchData, setFetchData] =
-    useState<GeneralisedHistoryType[]>(initialValue);
+    useState<GeneralisedHistoryTypeData>(initialObjectValue);
 
-  const cryptos = ["bitcoin", "ethereum"];
-
-  useEffect(() => {
-    getHistoryById(cryptos[0])
+  const getHistoryData = async (crypto: string) => {
+    return await getHistoryById(crypto)
       .then(getLast90Days)
+      .then(filterHistory)
       .then((data) => {
-        const result = filterHistory(data);
-
-        return result;
-      })
-      .then((data) => {
-        setFetchData(data);
-        setFetchStatus(true);
+        setFetchData({ ...fetchData, [crypto]: [...data] });
       })
       .catch((err) => console.log("ERR ", err));
-  }, []);
-  return <>{fetchStatus && <PriceHistory cryptoData={fetchData} />}</>;
+  };
+
+  useEffect(() => {
+    Promise.all([getHistoryData(cryptos[0]), getHistoryData(cryptos[1])])
+      .then(() => {
+        if (Object.values(fetchData).every((d) => d.length > 1)) {
+          setFetchStatus(true);
+          console.log(fetchData, " *^*^*^*^*^*^*^*^");
+        }
+      })
+      .catch((err) => console.log(err, " !!!! "));
+  }, [fetchStatus]);
+
+  return (
+    <Grid item sx={{ width: "100%" }}>
+      {fetchStatus && <PriceHistory cryptoData={fetchData} />}
+    </Grid>
+  );
 };
 
 export default HistoryContainer;
